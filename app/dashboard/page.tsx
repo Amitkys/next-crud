11e755d78bbe853babb93b57780924f30097b1de
KYs <amitkys59@gmail.com>
2024-10-28 22:58:27 +0530

    testing route for dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,33 @@
-export default function  () {
+"use client";
+// pages/index.tsx (or pages/index.js)
+import { useSession, signIn } from 'next-auth/react';
+
+const HomePage = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (!session) {
     return (
-        <div>
-            hi
-        </div>
-    )
-}
+      <div>
+        <h1>Welcome to the Home Page!</h1>
+        <p>Please sign in to access your account.</p>
+        <button onClick={() => signIn()}>Sign In</button>
+      </div>
+    );
+  }
+
+  // User is authenticated
+  return (
+    <div>
+      <h1>Welcome, {session.user.name}!</h1>
+      <p>{session.user.image}</p>
+      <img src={session.user.image} alt={session.user.name} />
+      <p>Email: {session.user.email}</p>
+    </div>
+  );
+};
+
+export default HomePage;
