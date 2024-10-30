9878aa7a4b224bff8a4ec66266e08e1744f318ab
KYs <amitkys59@gmail.com>
2024-10-30 17:32:04 +0530

    added navigation linke for go to all todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link"
 export default function homepage() {
   return (
     <div>
       <h1>Welcome to todo app</h1>
+      <Link href={'/todos'}>
+        <button>Show all Todos</button>
+      </Link>
     </div>
   )
 }
