21a81d8665aecbf8de8be072e31bf96a7df3ec15
KYs <amitkys59@gmail.com>
2024-10-30 17:27:07 +0530

    successfully fetched data in todos route

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,7 +1,12 @@
-export default function todos() {
-    return (
-        <div>
-            <h1>This is todos page</h1>
-        </div>
-    )
+import { ShowAllTodo } from "@/app/lib/Components/ShowAllTodo"
+import { fetchAllTodo } from "@/app/lib/actions";
+
+export default async function homePage() {
+  const data = await fetchAllTodo();
+  return (
+  <div>
+
+    <ShowAllTodo data = {data} />
+  </div>
+  )
 }
