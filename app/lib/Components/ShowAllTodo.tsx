d24be75cccec744852e6206f08e71a77487872df
KYs <amitkys59@gmail.com>
2024-10-30 18:16:52 +0530

    testing

--- app/lib/Components/ShowAllTodo.tsx
+++ app/lib/Components/ShowAllTodo.tsx
@@ -1,10 +1,12 @@
-"use client";
-export async function ShowAllTodo({ data }: {data: any[]}) {
+"use client"
+export  function ShowAllTodo({ data }: {data: any[]}) {
     return (
         <div>
             {data.map((todo) => (
                 <div key={todo.id}>
-                    <h1>{todo.title}</h1>
+                    <h1>Title: {todo.title}</h1>
+                    <p>Description: <strong> {todo.description}</strong> </p>
+                    <hr />
                 </div>
             ))}
         </div>
