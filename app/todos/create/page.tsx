082f9792a6c511bfe9e9185544aa746d79517bc6
KYs <amitkys59@gmail.com>
2024-11-01 13:50:48 +0530

    handle button

--- app/todos/create/page.tsx
+++ app/todos/create/page.tsx
@@ -1,14 +1,34 @@
-
+"use client";
+import { useState } from "react"
 import { createTodo } from "@/app/lib/actions"
 export default function () {
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        setLoading(true);
+
+        // create a FormData object to pass to the action
+        const formData = new FormData(event.target);
+
+        try {
+            await createTodo(formData); // action function
+        } catch (error) {
+            console.error("Error creating todo:", error);
+        } finally {
+            setLoading(false); // Re-enable the button after request
+        }
+    }
     return (
         <div>
-        <form action={createTodo}>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="title">Title</label>
             <input type="text" name="title" /> <br />
             <label htmlFor="description">Description</label>
             <input type="text" name="description" /> <br />
-            <button type="submit">CreateTodo</button>
+            <button type="submit" disabled={loading}>
+                {loading ? "processing..." : "Create Todo"}
+            </button>
         </form>
         </div>
     )
