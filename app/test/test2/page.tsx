34289e517ea8224ad674f39252a967a1414f4768
KYs <amitkys59@gmail.com>
2024-11-01 22:02:20 +0530

    commented test 2 function

--- app/test/test2/page.tsx
+++ app/test/test2/page.tsx
@@ -1,13 +1,13 @@
-"use client";
+// "use client";
 
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
-export default function test2(){
-    const router = useRouter();
-    return (
-        <div>
-            <h1>this is test 2</h1>
-            <button onClick={() => router.push('/todos')}>click</button>
-        </div>
-    )
-}
+// export default function test2(){
+//     const router = useRouter();
+//     return (
+//         <div>
+//             <h1>this is test 2</h1>
+//             <button onClick={() => router.push('/todos')}>click</button>
+//         </div>
+//     )
+// }
